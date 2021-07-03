76175111afdee5770134043a9c1e3af87b7fee47
Skiddie <domenp93@gmail.com>
2021-07-04 00:40:40 +0200

    api response handling

--- wp-content/plugins/joke-plugin/src/js/app.js
+++ wp-content/plugins/joke-plugin/src/js/app.js
@@ -1,8 +1,28 @@
 //Main body of the widget
 const joke = document.getElementById("joke")
+const date = new Date()
+const currentDay = date.getDay()
 
 //Fetching a joke
 async function jokes() {
   let data = await fetch("https://v2.jokeapi.dev/joke/Programming");
   let response = await data.json();
+  handleResponse(response)
+}
+
+//processing and storing fetch response
+function handleResponse(response) {
+  if (response["type"] == "twopart") {
+    let jokeInfo = {
+      "jokeday": date.getDay(),
+      "print": response["setup"] + '<br>' + response["delivery"]
+    }
+    localStorage.setItem("joke", JSON.stringify(jokeInfo))
+  } else {
+    let jokeInfo = {
+      "jokeday": date.getDay(),
+      "print": response["joke"]
+    }
+    localStorage.setItem("joke", JSON.stringify(jokeInfo))
+  }
 }
