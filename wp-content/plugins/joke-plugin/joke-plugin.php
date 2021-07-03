cecab66ec5021922be3f3bfe8d2f845eca292c74
Skiddie <domenp93@gmail.com>
2021-07-03 22:23:18 +0200

    plugin header meta

---
+++ wp-content/plugins/joke-plugin/joke-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Joke plugin
+Description: Displays joke of the day to user
+Version: 1.1.0
+Author: Domen
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+     exit; // Exit if accessed directly
+ }
