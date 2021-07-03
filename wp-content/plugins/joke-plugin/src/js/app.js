5771bc68efd11e10dfa61763c910499813d62086
Skiddie <domenp93@gmail.com>
2021-07-04 00:19:33 +0200

    joke fetching function

--- wp-content/plugins/joke-plugin/src/js/app.js
+++ wp-content/plugins/joke-plugin/src/js/app.js
@@ -1,2 +1,8 @@
-joke = document.getElementById("joke")
-joke.innerHTML = "lol";
+//Main body of the widget
+const joke = document.getElementById("joke")
+
+//Fetching a joke
+async function jokes() {
+  let data = await fetch("https://v2.jokeapi.dev/joke/Programming");
+  let response = await data.json();
+}
