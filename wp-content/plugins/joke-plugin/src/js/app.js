82de38f3f7fdef1d26d1b528faac205841cd1e5d
Skiddie <domenp93@gmail.com>
2021-07-04 12:31:51 +0200

    change handleResponse to switch statement

--- wp-content/plugins/joke-plugin/src/js/app.js
+++ wp-content/plugins/joke-plugin/src/js/app.js
@@ -12,18 +12,23 @@ async function jokes() {
 
 //processing and storing fetch response
 function handleResponse(response) {
-  if (response["type"] == "twopart") {
-    let jokeInfo = {
-      "jokeday": date.getDay(),
-      "print": response["setup"] + '<br>' + response["delivery"]
-    }
-    localStorage.setItem("joke", JSON.stringify(jokeInfo))
-  } else {
-    let jokeInfo = {
-      "jokeday": date.getDay(),
-      "print": response["joke"]
-    }
-    localStorage.setItem("joke", JSON.stringify(jokeInfo))
+  switch (response["type"]) {
+    case "twopart":
+       jokeInfo = {
+        "jokeday": date.getDay(),
+        "print": response["setup"] + '<br>' + response["delivery"]
+      }
+      localStorage.setItem("joke", JSON.stringify(jokeInfo))
+      break;
+    case "single":
+       jokeInfo = {
+        "jokeday": date.getDay(),
+        "print": response["joke"]
+      }
+      localStorage.setItem("joke", JSON.stringify(jokeInfo))
+      break;
+    default:
+
   }
 }
 
